608e2f14e002f2404825c497fbab2f9a72da500b
srijan023 <gyawalisrijan01@gmail.com>
2024-05-24 01:39:31 +0545

    Quality of life changes

--- lua/creatio/autocmds.lua
+++ lua/creatio/autocmds.lua
@@ -61,7 +61,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>gl", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
 
   -- might get dropped due to trouble
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
--- lua/creatio/plugins/telescope.lua
+++ lua/creatio/plugins/telescope.lua
@@ -7,8 +7,10 @@ return{
 		require("telescope").setup({})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 	end
 }
